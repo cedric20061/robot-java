193586625801105f7836a35d1e311ac1a2099912
Gérald <cedricguidi735@gmail.com>
2025-09-15 20:19:40 +0100

    feat: second project variant finished

--- src/Robot.java
+++ src/Robot.java
@@ -39,7 +39,7 @@ public class Robot {
 	/**
 	 * Vitesse de déplacement du Robot (unité : cases par seconde, ie m/s)
 	 */
-	private double vitesse = 2;
+	private double vitesse = 10;
 	/**
 	 * Indique si le robot est détruit ou non, un robot détruit ne peut plus se
 	 * déplacer
--- src/SimulateurAppX.java
+++ src/SimulateurAppX.java
@@ -6,19 +6,46 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class SimulateurAppX {
 
 	public static void main(String[] args) {
+		
+		String[][] sentences = {
+			    {"Choisir la langue", "Combien de victimes dois-je trouver ?", "Courage !", "La moyenne de gravité est : "},
+			    {"Choose language", "How many victims have I to save?", "Be strong!", "The average gravity is: "},
+			    {"Elige idioma", "¿Cuántas víctimas tengo que salvar?", "¡Sé fuerte!", "El promedio de gravedad es: "},
+			    {"Sprache wählen", "Wie viele Opfer muss ich retten?", "Sei stark!", "Der Durchschnitt der Schwere ist: "}
+			};
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("═══════════Language selection═══════════");
+		System.out.println("1- Francais\n2- English\n3- Spanish\n4- Allemand\n");
+		int language = 2;
+		do {
+			System.out.printf("%s \n", sentences[language-1][0]);
+			language = sc.nextInt();
+			
+		} while(language<=0 || language>4);
+		
+		int victimNumber = 0;
+		do {
+			System.out.printf("%s \n", sentences[language-1][1]);
+			victimNumber = sc.nextInt();
+			
+		} while(victimNumber<=0 || victimNumber>6);
+		
 		// création de l'environnement et récupération du terrain
 		Terrain terrain = Environnement.creerEnvironnement(10, 10);
 
 		// création du robot
-		Robot robot = new Robot(8, 7, "sud");
+		Robot robot = new Robot(0, 0, "sud");
 
 		// ajout du robot sur le terrain
 		terrain.ajouterRobot(robot);
-		terrain.ajouterPlusieursVictimes();
+		terrain.ajouterNombreDeVictimes(victimNumber);
 		
 		// mise à jour des composants graphiques
 		
@@ -29,24 +56,50 @@ public class SimulateurAppX {
 
 		List<Integer> victimsFoundGravity = new ArrayList<>();
 		
+		int victimFound = 0;
 		while(true) {
-			robot.avancer();
-			if(robot.isSurVictime()) {
-				victimsFoundGravity.add(robot.detecterGravite());
-				
+			
+			if(victimFound == victimNumber) {
+				break;
 			}
+			
+			victimFound += avancerEtVerifier(robot, victimsFoundGravity);
 			if(robot.getLigne() == 9 || robot.getLigne() == 0) {
 				if (robot.getColonne() == 9 && (robot.getLigne() == 0 || robot.getLigne() == 9)) {
 					break;
 				}
 				
 				if (robot.getLigne() == 9 || robot.getLigne() == 0) {
-			        changeLine(robot);
+			        victimFound += changeLine(robot, victimsFoundGravity);
 			    }
 				
 			}
+			
 		}
 		
+		int higherGravity = max(victimsFoundGravity);
+		robot.tournerDroite();
+		robot.tournerDroite();
+		int occurency = countOccurency(victimsFoundGravity, higherGravity);
+		int count=0;
+		while(true) {
+			if(robot.isSurVictime()) {
+				if(robot.detecterGravite() == higherGravity) {
+					count++;
+					System.out.printf("%s !\n", sentences[language-1][2]);
+					JOptionPane.showMessageDialog(null, sentences[language-1][2]);
+				}
+			}
+			if(count == occurency)
+				break;
+			robot.avancer();
+			if(robot.getLigne() == 0 && robot.getColonne() == 0) {
+				break;
+			}
+			if (robot.getLigne() == 9 || robot.getLigne() == 0) {
+				changeLineReturn(robot);
+		    }
+		}
 		if(victimsFoundGravity.size() >0) {
 			System.out.println("═════════════════ Victims List ═════════════════");
 			System.out.println("╔════════════╦═════════════════╗");
@@ -60,24 +113,69 @@ public class SimulateurAppX {
 
 			System.out.println("╚════════════╩═════════════════╝");
 			float gravityMoy = (float) gravityTotal/victimsFoundGravity.size();
-			System.out.printf("The average gravity is: %f", gravityMoy);
+			System.out.printf("%s %f",sentences[language-1][3], gravityMoy);
 
 		}else {
 			System.out.println("No victim found");
 		}
-		
+		sc.close();
 	}
 	
-	private static void changeLine(Robot robot) {
+	private static int changeLine(Robot robot, List<Integer> victimsFoundGravity) {
+	    int ans = 0;
 	    if (robot.getLigne() == 9) {
 	        robot.tournerGauche();
-	        robot.avancer();
+	        ans += avancerEtVerifier(robot, victimsFoundGravity);
 	        robot.tournerGauche();
 	    } else {
 	        robot.tournerDroite();
-	        robot.avancer();
+	        ans += avancerEtVerifier(robot, victimsFoundGravity);
 	        robot.tournerDroite();
 	    }
+	    return ans;
+	}
+
+	
+	private static void changeLineReturn(Robot robot) {
+	    if (robot.getLigne() == 9) {
+	    	robot.tournerDroite();
+	        robot.avancer();
+	        robot.tournerDroite();
+	        
+	    } else {
+	    	robot.tournerGauche();
+	        robot.avancer();
+	        robot.tournerGauche();
+	    }
+	}
+	
+	private static int max(List<Integer> victimsFoundGravity) {
+		int max=0;
+		for(int i=0; i<victimsFoundGravity.size(); i++) {
+			if(victimsFoundGravity.get(i)>max) {
+				max = victimsFoundGravity.get(i);
+			}
+		}
+		return max;
+	}
+	
+	private static int avancerEtVerifier(Robot robot, List<Integer> victimsFoundGravity) {
+	    robot.avancer();
+	    if (robot.isSurVictime()) {
+	        victimsFoundGravity.add(robot.detecterGravite());
+	        return 1;
+	    }
+	    return 0;
+	}
+
+	private static int countOccurency(List<Integer> list, int num) {
+		int occurency=0;
+		for(int i=0; i<list.size(); i++) {
+			if(list.get(i) == num) {
+				occurency++;
+			}
+		}
+		return occurency;
 	}
 
 }
